ce6186d441fd536abfe1f9395a1e983fdfdd6ac4
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-29 23:19:08 -0400

    JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -14,7 +16,14 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = jwt.sign(
+    tokenUser,
+    process.env.JWT_SECRET,
+    process.env.JWT_LIFETIME
+  );
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send('login user');
