5eee2ae2fe69515fe748d94bc2d957c2bb2326ce
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-29 23:29:59 -0400

    Refactor JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const { createJWT } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -17,11 +16,7 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password, role });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  const token = jwt.sign(
-    tokenUser,
-    process.env.JWT_SECRET,
-    process.env.JWT_LIFETIME
-  );
+  const token = createJWT({ payload: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
