95ce222c9dc3630a70fa15d0d6a7d248a1f98d43
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-31 22:11:19 -0400

    CreateTokenUser Function

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,8 @@
 const { createJWT, isTokenValid, attchCookiesToResponse } = require('./jwt');
-
+const createTokenUser = require('./createTokenUser');
 module.exports = {
   createJWT,
   isTokenValid,
   attchCookiesToResponse,
+  createTokenUser,
 };
