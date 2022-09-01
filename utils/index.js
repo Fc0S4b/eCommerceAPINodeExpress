e88a88e6064e4cd3ad41acc25723b6978220be96
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-31 23:17:01 -0400

    CheckPermissions function

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,10 @@
 const { createJWT, isTokenValid, attchCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
+const checkPermissions = require('./checkPermissions');
 module.exports = {
   createJWT,
   isTokenValid,
   attchCookiesToResponse,
   createTokenUser,
+  checkPermissions,
 };
