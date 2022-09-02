96f9035da4d50f33377319ae2b2b76030c681d3d
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-01 23:29:59 -0400

    Populate Method

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -28,7 +28,15 @@ const createReview = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({}).populate({
+    path: 'product',
+    select: 'name company price',
+  });
+  // también para user, se puede agregar mas populate
+  // .populate({
+  //   path: 'user',
+  //   select: 'name',
+  // });
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
 const getSingleReview = async (req, res) => {
